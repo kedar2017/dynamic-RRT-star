e7b137453bf4c5fa29a797d414bc2006b56571a9
Kedar <Joshi>
2021-06-19 16:43:22 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <memory>
+#include <unistd.h>
+using namespace std;
+
+int main(int argc, char* argv[]){
+    cout << "Starting program" << endl;
+    return 0;
+}
